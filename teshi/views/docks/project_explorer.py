e379deb29ff6f2a6c70fcad26c1b0de36794dd89
lilinfang <lilinfangrelax@gmail.com>
2025-12-07 14:38:57 +0800

    Add folder and file icons to project explorer items

--- teshi/views/docks/project_explorer.py
+++ teshi/views/docks/project_explorer.py
@@ -1,6 +1,6 @@
 import os
 from PySide6.QtWidgets import QApplication, QTreeView
-from PySide6.QtGui import QStandardItemModel, QStandardItem
+from PySide6.QtGui import QStandardItemModel, QStandardItem, QIcon
 
 
 class ProjectExplorer(QTreeView):
@@ -12,24 +12,33 @@ class ProjectExplorer(QTreeView):
 
         root_item = QStandardItem(os.path.basename(target_dir))
         root_item.setEditable(False)
+        root_item.setIcon(QIcon("assets/icons/folder.png"))
         populate_tree(root_item, target_dir)
 
         model = QStandardItemModel()
         model.appendRow(root_item)
         self.setModel(model)
 
-
-
 def populate_tree(parent_item, path):
     try:
         for entry in os.listdir(path):
             full_path = os.path.join(path, entry)
-            item = QStandardItem(entry)
-            item.setEditable(False)
-            parent_item.appendRow(item)
-
             if os.path.isdir(full_path):
+                item = QStandardItem(entry)
+                item.setEditable(False)
+                item.setIcon(QIcon("assets/icons/folder.png"))
+                parent_item.appendRow(item)
                 populate_tree(item, full_path)
+            else:
+                item = QStandardItem(entry)
+                item.setEditable(False)
+                # if file name ends with .md, use testcase_normal icon
+                if full_path.endswith(".md"):
+                    item.setIcon(QIcon("assets/icons/testcase_normal.png"))
+                else:
+                    item.setIcon(QIcon("assets/icons/unknown_file.png"))
+                parent_item.appendRow(item)
+
     except PermissionError:
         pass
 
